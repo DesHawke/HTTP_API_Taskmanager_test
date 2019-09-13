94f4c3db22fa5ac647d0b3fccc0dadfd9eeb20a7
DesHawke <salomatov_96@mail.ru>
2019-09-13 14:11:32 +0700

    Basic Auth (работает)

--- src/main/kotlin/com/example/taskmanager/WebSecurityConfig.kt
+++ src/main/kotlin/com/example/taskmanager/WebSecurityConfig.kt
@@ -1,35 +1,31 @@
 package com.example.taskmanager
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    fun encoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 
-    @Bean
-    @Throws(Exception::class)
-    override fun authenticationManagerBean(): AuthenticationManager {
-        return super.authenticationManagerBean()
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(encoder().encode("adminPass")).roles("ADMIN")
+                .and()
+                .withUser("user").password(encoder().encode("userPass")).roles("USER")
     }
 
-    @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
         http.httpBasic()
                 .and()
@@ -38,7 +34,7 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .exceptionHandling()
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/tasks", "/tag/{id}").permitAll()
+                .antMatchers(HttpMethod.GET, "/tasks", "/tag/**").permitAll()
                 .antMatchers(HttpMethod.DELETE).authenticated()
                 .antMatchers(HttpMethod.POST).authenticated()
                 .anyRequest().authenticated()
@@ -47,15 +43,4 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
-    @Throws(Exception::class)
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                .password("{noop}password")
-                .authorities("USER")
-                .and()
-                .withUser("user2")
-                .password("{noop}admin")
-                .authorities("ADMIN")
-    }
 }
