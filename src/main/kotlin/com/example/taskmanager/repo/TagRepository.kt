fb970581ccff278b963f8939b06065646a748361
DesHawke <salomatov_96@mail.ru>
2019-09-16 13:50:20 +0700

    Добавлена функция проверки существующего имени

--- src/main/kotlin/com/example/taskmanager/repo/TagRepository.kt
+++ src/main/kotlin/com/example/taskmanager/repo/TagRepository.kt
@@ -1,9 +1,14 @@
 package com.example.taskmanager.repo
 
+import ch.qos.logback.core.joran.spi.ConsoleTarget
+import ch.qos.logback.core.joran.spi.ConsoleTarget.findByName
 import com.example.taskmanager.model.Tag
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-
 @Repository
-interface TagRepository : JpaRepository<Tag, Long>
+interface TagRepository : JpaRepository<Tag,Long>{
+    fun existsByName(name: String):Boolean{
+        return findByName(name) !=null
+    }
+}
--- src/main/kotlin/com/example/taskmanager/repo/TaskRepository.kt
+++ src/main/kotlin/com/example/taskmanager/repo/TaskRepository.kt
@@ -1,12 +1,13 @@
 package com.example.taskmanager.repo
 
+import ch.qos.logback.core.joran.spi.ConsoleTarget.findByName
 import com.example.taskmanager.model.Task
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-
 @Repository
-interface TaskRepository : JpaRepository<Task, Long> {
-    fun findAllByTagId(tagId: Long):List<Task>
+interface TaskRepository : JpaRepository<Task,Long> {
+    fun existsByName(name: String):Boolean{
+        return findByName(name)!=null
+    }
 }
